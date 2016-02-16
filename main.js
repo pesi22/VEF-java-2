683ca031ce48ca6fdfcab8bfeaccb444598fdab6
Pétur Magnús <peturpe309@nemi.tskoli.is>
2016-02-16 20:52:17 +0000

    eg setti meirimetnað i þetta
    eg setti meirimetnað i þetta

--- main.js
+++ main.js
@@ -1,52 +1,30 @@
-"use strict";
+"use strict"
+ function pizza (name,topping,price) {
+	// body...
+	
+	this.name = name;
+	this.topping = topping;
+	this.price = price;
+
+}
+
+var pizzas = [['Margarita','Ostur og kridd','L 1500 - M 1250 - S 1000'],['Pepperoni Veisla','Extra Pepperoni og Ostur','L 1500 - M 1250 - S 1000'], ['Beikon sigir gagalagú ','Kjulli og Beikon','L 1500 - M 1250 - S 1000'],['Þetta er Pizza','Insert Pizza shit here','L 1500 - M 1250 - S 1000'] , ['Mamma special','Lambakjöt og sulta','L 1500 - M 1250 - S 1000'],['Banani og Laukur','Nákvæmlega það sem hún heitir','L 1500 - M 1250 - S 1000'], ['Ekki borða svín ','Pulled pork og beikon ','L 1500 - M 1250 - S 1000'],['Tenage mutend ninja turtles','Sikur púði og rækjur ','L 1500 - M 1250 - S 1000'] ];
+
+var pizzaRun = [];
 
 
-var spurning = "Hvernig pizzu villtu ?";
 
-var elSpurning = document.getElementById("question");
 
-// Notum DOM JS aðferð textContent til að færa streng yfir í index.html
-elSpurning.textContent = spurning;
+	for (var i = pizzas.length - 1; i >= 0; i--) {
+	pizzaRun[i] = new pizza(pizzas[i][0] , pizzas[i][1], pizzas[i][2]);
+};
 
-// DOM innerHTML JS aðferð getur blandað saman streng og html element  
-elSpurning.innerHTML = "<em>" + spurning + "</em>";
 
-var val1 = "DOMINO'S CLASSIC - pepperoni, græn paprika, laukur, sveppir";
- var elval1 =document.getElementById("val-1");
- elval1.textContent = val1;
- elval1.innerHTML = " <input type = checkbox>" + val1 + " <br>  lítil <input type = radio >  mið <input type = radio> stór <input type = radio>" ;
+	var echopizza = document.getElementById("demo");
 
- var val2 = "DOMINO'S CLASSIC ++ - Auka pepperoni, Auka græn paprika, Auka laukur, Auka sveppir";
- var elval2 =document.getElementById("val-2");
- elval2.textContent = val2;
- elval2.innerHTML = " <input type = checkbox>" + val2 + " <br>  lítil <input type = radio >  mið <input type = radio> stór <input type = radio>" ;
+	for (var i = pizzaRun.length - 1; i >= 0; i--) {
+	echopizza.innerHTML += "<h1 > " + pizzaRun[i].name + "</h1>" + "<h3>" + pizzaRun[i].topping + "</h3>" + "<h5>" +  pizzaRun [i].price + "</h5>";
+};
 
-var val3 = "FESTIVAL - pepperoni, rjómaostur, jalapeno, nachos flögur, rauðlaukur, fajitas kjúklingur";
- var elval3 =document.getElementById("val-3");
- elval3.textContent = val3;
- elval3.innerHTML = " <input type = checkbox>" + val3 + " <br>  lítil <input type = radio >  mið <input type = radio> stór <input type = radio>" ;
 
-var val4 = "SMOKY BEARNAISE - hakk, cheddarostur, oregano, laukur, rauðlaukur, sveppir, chili-bearnaise";
- var elval4 =document.getElementById("val-4");
- elval4.textContent = val3;
- elval4.innerHTML = " <input type = checkbox>" + val4 + " <br>  lítil <input type = radio >  mið <input type = radio> stór <input type = radio>" ;
 
-var val5 = "ÍTALÍA - ferskur mozzarella, kirsuberjatómatar, basilpestó";
- var elval5 =document.getElementById("val-5");
- elval5.textContent = val5;
- elval5.innerHTML = " <input type = checkbox>" + val5 + " <br>  lítil <input type = radio >  mið <input type = radio> stór <input type = radio>" ;
-
-var val6 = "NEW YORK - hakk, beikonkurl, cheddarostur";
- var elval6 =document.getElementById("val-6");
- elval6.textContent = val6;
- elval6.innerHTML = " <input type = checkbox>" + val6 + " <br>  lítil <input type = radio >  mið <input type = radio> stór <input type = radio>" ;
-
-var val7 = "KJÖTVEISLA - pepperoni, skinka, hakk";
- var elval7 =document.getElementById("val-7");
- elval7.textContent = val7;
- elval7.innerHTML = " <input type = checkbox>" + val7 + " <br>  lítil <input type = radio >  mið <input type = radio> stór <input type = radio>" ;
-
-var val8 = "PEPPERONI VEISLA - pepperoni, auka ostur";
- var elval8 =document.getElementById("val-8");
- elval8.textContent = val8;
- elval8.innerHTML = " <input type = checkbox>" + val8 + " <br>  lítil <input type = radio >  mið <input type = radio> stór <input type = radio>" ;
